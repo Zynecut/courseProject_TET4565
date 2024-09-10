e73b75d666e9e329f335ec38fb08434e875b1260
AleksanderVS <106428709+Zynecut@users.noreply.github.com>
2024-09-10 10:29:05 +0200

    Started with main structure

--- main.py
+++ main.py
@@ -1,16 +1,73 @@
-# This is a sample Python script.
+# Imports
+import numpy as np
+import pandas as pd
+import pyomo.environ as pyo
+from pyomo.opt import SolverFactory
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Structure
+
+"""
+generator data: capacity, marg_cost, location, slackbus, CO2_emission
+
+load data: demand, marg_WTP, location
+
+transmission data, capacity, susceptance
+"""
+
+def main():
+    # read data from excel files
+    file_name = 'data.xlsx'
+    data = read_data(file_name)
+    B_matrix = make_Bmatrix(data)
+    DCOPF_Model(data, B_matrix)
+    return data
+
+def read_data(file):
+    # read data from file
+    data = {}
+    df = pd.read_excel
+    return data
+    
+    
+def make_Bmatrix(data):
+    length = len(data[task]['Line']['Susceptance [p.u]'])               # Get the length of the B matrix
+    B_matrix = np.zeros((length, length))                               # Create empty matrix
+    return B_matrix
+    
+def fix_Data(data):
+    # fix the data to be in the right format
+    return data
+    
+def DCOPF_Model(data, B_matrix):
+    """
+    Setup the optimization model, run it and store the data in a .xlsx file
+    """
+    model = pyo.ConcreteModel() #Establish the optimization model, as a concrete model in this case
+
+    """
+    Sets
+    """
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    """
+    Parameters
+    """
 
 
-# Press the green button in the gutter to run the script.
+    """
+    Variables
+    """
+
+
+    """
+    Objective Function
+    """
+
+
+    return()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
