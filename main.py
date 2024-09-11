04ca89dc81fb616821a1cb6319337376bfbb5857
AleksanderVS <106428709+Zynecut@users.noreply.github.com>
2024-09-11 14:21:14 +0200

    Started with main structure

--- main.py
+++ main.py
@@ -12,27 +12,47 @@ generator data: capacity, marg_cost, location, slackbus, CO2_emission
 load data: demand, marg_WTP, location
 
 transmission data, capacity, susceptance
+
+
+Datasett Producers comment
+Fuelcost neglected for wind, solar and hydro
+Inflow_factor neglected (hydro)
+Storage price neglected. (hydro)
+Initial storage neglected. (hydro)
+Marginal_cost is just a wild guess
+
+
+Datasett Consumers comment
+Add flexible consumers (WTP)?
+
+
+Datasett Time comment
+Load is given as a ratio of demand
+Wind is given as a ratio of p_max
+Solar is given as a ratio of p_max
+
+
+Datasett Node comment
+Only for geographical plotting 
+
 """
 
 def main():
     # read data from excel files
-    file_name = 'data.xlsx'
+    file_name = 'Datasett_NO1_Cleaned_r2.xlsx'
     data = read_data(file_name)
-    B_matrix = make_Bmatrix(data)
     DCOPF_Model(data, B_matrix)
     return data
 
 def read_data(file):
     # read data from file
     data = {}
-    df = pd.read_excel
+    excel_sheets = ['Producers', 'Consumers', 'Time', 'Node']
+    for sheet in excel_sheets:
+        df = pd.read_excel(file, sheet_name=sheet)
+        data[sheet] = df.to_dict(orient='list')
     return data
-    
-    
-def make_Bmatrix(data):
-    length = len(data[task]['Line']['Susceptance [p.u]'])               # Get the length of the B matrix
-    B_matrix = np.zeros((length, length))                               # Create empty matrix
-    return B_matrix
+
     
 def fix_Data(data):
     # fix the data to be in the right format
