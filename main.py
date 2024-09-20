218f150ec8a94f997ad2a591c8a296d151b3cb2a
MyhraEngineering <simenmd@stud.ntnu.no>
2024-09-20 13:33:03 +0200

    test

--- main.py
+++ main.py
@@ -1,56 +1,10 @@
-# Imports
-import numpy as np
 import pandas as pd
 import pyomo.environ as pyo
 from pyomo.opt import SolverFactory
-from datetime import datetime
-from collections import defaultdict
-
-# Structure
-
-"""
-generator data: capacity, marg_cost, location, slackbus, CO2_emission
-
-load data: demand, marg_WTP, location
-
-transmission data, capacity, susceptance
 
 
-Datasett Producers comment
-Fuelcost neglected for wind, solar and hydro
-Inflow_factor neglected (hydro)
-Storage price neglected. (hydro)
-Initial storage neglected. (hydro)
-Marginal_cost is just a wild guess
-
-
-Datasett Consumers comment
-Add flexible consumers (WTP)?
-
-
-Datasett Time comment
-Load is given as a ratio of demand
-Wind is given as a ratio of p_max
-Solar is given as a ratio of p_max
-
-
-Datasett Node comment
-Only for geographical plotting 
-
-"""
-
-def main():
-    # read data from excel files
-    file_name = 'Datasett_NO1_Cleaned_r4.xlsx'
-    data = inputData(file_name)
-
-    # DCOPF_Model(data) #, b_matrix)
-    return()
-
-
-
+# Function to load data including wind data
 def inputData(file):
-    # read data from file
     data = {}
     excel_sheets = ['Producers', 'Consumers', 'Time_wind', 'Node']
     for sheet in excel_sheets:
@@ -59,39 +13,91 @@ def inputData(file):
     return data
 
 
-
-def modelSetup(data):# , b_matrix):
-    """
-    Set up the optimization model, run it and store the data in a .xlsx file
-    """
-    m = pyo.ConcreteModel() #Establish the optimization model, as a concrete model in this case
-
-    """
-    Sets
-    """
-    m.Producers = pyo.Set(initialize=xxx)  # Generators
-    m.Consumers = pyo.Set(initialize=xxx)  # Load units
-    m.Nodes = pyo.Set(initialize=xxx)  # Nodes
-
-    """
-    Parameters
-    """
+# Use the inputData function to load data
+data = inputData('Datasett_NO1_Cleaned_r4.xlsx')
 
 
-    """
-    Variables
-    """
+# Objective function to minimize total cost of generation
+def ObjectiveFunction(m):
+    return sum(m.C[i] * m.x[i] for i in m.I)
 
 
-    """
-    Objective Function
-    """
+# Constraints: generation limits with wind factor
+def GenerationUpperLimit(m, i):
+    # Find the index of the producer in the data
+    index = data['Producers']['Producer'].index(i)
+
+    # Check if the producer is wind-based
+    if data['Producers']['type'][index] == 'wind':
+        wind_ratio = data['Time_wind']['wind_med'][0]  # Use 'wind_med' or any wind data column
+        return m.x[i] <= wind_ratio * m.pmax[i]
+    else:
+        return m.x[i] <= m.pmax[i]
 
 
-    return()
+def GenerationLowerLimit(m, i):
+    return m.x[i] >= m.pmin[i]
 
 
-if __name__ == '__main__':
-    main()
+# Load constraints (matching consumer demand)
+def LoadConstraint(m, j):
+    return sum(m.x[i] for i in m.I) >= m.demand[j]
 
 
+# Model setup with wind production limits
+def ModelSetUpMinCost(data):
+    m = pyo.ConcreteModel()
+
+    # Define sets
+    producers = data['Producers']['Producer']
+    consumers = data['Consumers']['nodeID']
+
+    m.I = pyo.Set(initialize=producers)  # Producers
+    m.J = pyo.Set(initialize=consumers)  # Consumers
+
+    # Define parameters for producers
+    pmax_dict = dict(zip(data['Producers']['Producer'], data['Producers']['pmax']))
+    pmin_dict = dict(zip(data['Producers']['Producer'], data['Producers']['pmin']))
+    marginal_cost_dict = dict(zip(data['Producers']['Producer'], data['Producers']['marginal_cost']))
+
+    m.pmax = pyo.Param(m.I, initialize=pmax_dict)
+    m.pmin = pyo.Param(m.I, initialize=pmin_dict)
+    m.C = pyo.Param(m.I, initialize=marginal_cost_dict)
+
+    # Define parameters for consumers (demand from consumption)
+    demand_dict = dict(zip(data['Consumers']['nodeID'], data['Consumers']['consumption']))
+    m.demand = pyo.Param(m.J, initialize=demand_dict, within=pyo.Reals)
+
+    # Define variables for generation
+    m.x = pyo.Var(m.I, within=pyo.NonNegativeReals)
+
+    # Define constraints
+    m.GenUpperLimit = pyo.Constraint(m.I, rule=GenerationUpperLimit)
+    m.GenLowerLimit = pyo.Constraint(m.I, rule=GenerationLowerLimit)
+
+    # Load constraint: Ensure the sum of generation meets the load
+    m.LoadConstraint = pyo.Constraint(m.J, rule=LoadConstraint)
+
+    # Define objective function
+    m.obj = pyo.Objective(rule=ObjectiveFunction, sense=pyo.minimize)
+
+    return m
+
+
+# Solve function
+def SolveModel(m):
+    opt = SolverFactory("gurobi")
+    m.dual = pyo.Suffix(direction=pyo.Suffix.IMPORT)
+    results = opt.solve(m, load_solutions=True)
+    return results, m
+
+
+# Display results
+def DisplayModelResults(m):
+    return m.pprint()
+
+
+# Model setup and solve
+m = ModelSetUpMinCost(data)
+results, m = SolveModel(m)
+DisplayModelResults(m)
