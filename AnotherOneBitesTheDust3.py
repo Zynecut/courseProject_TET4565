cd65826dddccb56fa318948cf2c907f8b318c343
AleksanderVS <106428709+Zynecut@users.noreply.github.com>
2024-10-02 16:55:57 +0200

    many tries

--- AnotherOneBitesTheDust3.py
+++ AnotherOneBitesTheDust3.py
@@ -21,7 +21,8 @@ def main():
     file_name = 'Datasett_NO1_Cleaned_r5.xlsx'
     data = inputData(file_name)
     pprint.pprint(data, width=1)
-    model = modelSetup_1(data)
+    const = {'p_res': 20, 'cost_res':25}
+    model = modelSetup_1(data, const)
     results, model = SolveModel(model)
     DisplayModelResults(model)
 
@@ -65,37 +66,41 @@ def limit_nuclear_DA(m):
     return m.P_min["nuclear"], m.P_max["nuclear"]  # Begrenser produksjonen til maksimal produksjon for kjernekraft.
 def limit_hydro_DA(m):
     return m.P_min["hydro"], m.P_max["hydro"]
-def limit_wind_DA(m):
-    return m.P_min["wind"], m.P_max["wind"]
+# def limit_wind_DA(m):
+#     return m.P_min["wind"], m.P_max["wind"]
 def limit_nuclear_RT(m, s):
     return m.P_min["nuclear"], m.P_max["nuclear"]
 def limit_hydro_RT(m, s):
     return m.P_min["hydro"], m.P_max["hydro"]
-def limit_wind_RT(m, s):
-    return m.P_min["wind"], m.P_max["wind"]
+# def limit_wind_RT(m, s):
+#     return m.P_min["wind"], m.P_max["wind"]
 def rationing_limits(m, l, s):
     return 0, m.demand[l]
 
 """Constraints"""
+# def wind_DA_constraint(m, s):
+#     return m.wind_DA[s] <= m.P_wind[s]
 def wind_surplus_constraint(m, s):
-    return m.wind_sur[s] >= m.wind_DA - m.P_wind[s]
+    return m.wind_sur[s] == m.wind_DA[s] - m.wind_RT[s] # m.wind_DA - m.P_wind[s]
 def locked_nuclear_prod(m, s):
-    return m.nuclear_DA == m.nuclear_RT[s]
+    return m.nuclear_RT[s] == m.nuclear_DA
 def load_balance_DA(m):
-    return m.nuclear_DA + m.hydro_DA + m.wind_DA >= m.demand["Load 1"]
+    return m.nuclear_DA + m.hydro_DA + m.wind_DA['med'] >= m.demand["Load 1"]
 def load_balance_RT(m, s):
-    return m.hydro_RT[s] + m.wind_RT[s] + m.rationing["Load 1", s] == m.demand["Load 1"]
+    return m.hydro_RT[s] + (m.wind_RT[s] - m.wind_DA[s] - m.wind_sur[s]) + m.rationing["Load 1", s] == 0
+
 
 """Objective Function"""
 def ObjFunction(m):
-    production_cost_DA = m.MC['nuclear'] * m.nuclear_DA + m.MC['hydro'] * m.hydro_DA + m.MC['wind'] * m.wind_DA
-    production_cost_RT = sum(m.prob[s] * (m.MC['nuclear'] * m.nuclear_RT[s] + m.MC['hydro'] * m.hydro_RT[s] + m.MC['wind'] * m.wind_RT[s]) for s in m.S)
+    production_cost_DA = m.MC['nuclear'] * m.nuclear_DA + m.MC['hydro'] * m.hydro_DA + m.MC['wind'] * m.wind_DA['med']
+    # production_cost_RT = sum(m.prob[s] * (m.MC['nuclear'] * m.nuclear_RT[s] + m.MC['hydro'] * m.hydro_RT[s] + m.MC['wind'] * m.wind_RT[s]) for s in m.S)
+    production_cost_RT = sum(m.prob[s] * (m.MC['hydro'] * m.hydro_RT[s]) for s in m.S)
     rationing_cost = sum(m.prob[s] * m.cost_rat[l] * m.rationing[l, s] for l in m.L for s in m.S)
     risk_cost = sum(m.prob[s] * 50 * m.wind_sur[s] for s in m.S)                       # Prøvde å legge inn en straffekostnad for overskuddsvind
     return production_cost_DA + production_cost_RT + rationing_cost + risk_cost
 
 
-def modelSetup_1(data):
+def modelSetup_1(data, const):
     m = pyo.ConcreteModel() #Establish the optimization model, as a concrete model in this case
     """Sets"""
     m.G =           pyo.Set(initialize=list(data['Producers']['p_max'].keys()))  # ('nuclear', 'hydro', 'wind')
@@ -108,20 +113,23 @@ def modelSetup_1(data):
     m.MC =          pyo.Param(m.G, initialize=data['Producers']['marginal_cost'])
     m.demand =      pyo.Param(m.L, initialize=data['Consumers']['consumption'])
     m.cost_rat =    pyo.Param(m.L, initialize=data['Consumers']['rationing_cost'])
-    m.P_wind =      pyo.Param(m.S, initialize=data['Time_wind'])
+    # m.P_wind =      pyo.Param(m.S, initialize=data['Time_wind'])
+    m.wind_DA =     pyo.Param(m.S, initialize=data['Time_wind'])
     m.prob =        pyo.Param(m.S, initialize={'low': 0.8, 'med': 0.1, 'high': 0.1})
+    m.wind_RT =     pyo.Param(m.S, initialize={'low': 30, 'med': 30, 'high': 30})
 
     """Variables"""
     m.nuclear_DA =  pyo.Var(bounds=limit_nuclear_DA, within=pyo.NonNegativeReals)
     m.hydro_DA =    pyo.Var(bounds=limit_hydro_DA, within=pyo.NonNegativeReals)
-    m.wind_DA =     pyo.Var(bounds=limit_wind_DA, within=pyo.NonNegativeReals)
+    # m.wind_DA =     pyo.Var(m.S, bounds=limit_wind_DA, within=pyo.NonNegativeReals)
     m.nuclear_RT =  pyo.Var(m.S, bounds=limit_nuclear_RT, within=pyo.NonNegativeReals)
     m.hydro_RT =    pyo.Var(m.S, bounds=limit_hydro_RT, within=pyo.NonNegativeReals)
-    m.wind_RT =     pyo.Var(m.S, bounds=limit_wind_RT, within=pyo.NonNegativeReals)
+    # m.wind_RT =     pyo.Var(m.S, bounds=limit_wind_RT, within=pyo.NonNegativeReals)
     m.wind_sur =    pyo.Var(m.S, within=pyo.NonNegativeReals)
     m.rationing =   pyo.Var(m.L, m.S, bounds=rationing_limits, within=pyo.NonNegativeReals)  # Setter øvre og nedre begrensning for rasjonering direkte i variabelen ved initialisering.
 
     """Constraints"""
+    # m.WindDAConstraint =        pyo.Constraint(m.S, rule=wind_DA_constraint)
     m.WindSurplusConstraint =   pyo.Constraint(m.S, rule=wind_surplus_constraint)
     m.LockedNuclearProd =       pyo.Constraint(m.S, rule=locked_nuclear_prod)
     m.LoadBalance_DA =          pyo.Constraint(rule=load_balance_DA)
